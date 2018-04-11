fc4f76b84422a33e70e16410333c3e250c77d791
Nick Dewey <dewey.71@osu.edu>
2018-04-11 00:19:50 -0400

    remove missile to free resources when it is too far from the player to impact the game

--- Assets/scripts/Missile.cs
+++ Assets/scripts/Missile.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour {
 
+	public Transform playerTransform;
+
+	private int maxDistanceFromPlayer = 600;
+
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//remove missile to free resources when it is too far from the player to impact the game
+		if (Vector3.Distance (playerTransform.position, transform.position) > maxDistanceFromPlayer) {
+			Destroy (gameObject);
+		}
 	}
 
 	public void OnTriggerEnter2D(Collider2D collider){
