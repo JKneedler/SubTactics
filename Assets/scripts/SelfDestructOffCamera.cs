bce94b35f2b4062b07d2c50d287419a4ee76be7b
Nick Dewey <dewey.71@osu.edu>
2018-04-18 00:49:34 -0400

    cleanup mine game objects as they fall behind the camera

---
+++ Assets/scripts/SelfDestructOffCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructOffCamera : MonoBehaviour {
+
+	private Camera mainCamera;
+	private float maxDistanceFromCamera;
+	// Use this for initialization
+	void Start () {
+		mainCamera = Camera.main;
+		maxDistanceFromCamera = mainCamera.orthographicSize*5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+ 		if ((transform.position - mainCamera.transform.position).magnitude > maxDistanceFromCamera) {
+			Destroy (gameObject);
+		}
+	}
+}
